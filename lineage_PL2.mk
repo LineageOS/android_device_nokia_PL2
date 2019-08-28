d1d9738d1d455d84ef9460d99f1c7500737f3ddc
theimpulson <aayushgupta219@gmail.com>
2019-08-28 02:17:02 -0400

    PL2: Initial Commit

---
+++ lineage_PL2.mk
@@ -0,0 +1,57 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common AOSP stuff
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from nokia sdm660-common
+$(call inherit-product, device/nokia/sdm660-common/sdm660.mk)
+
+# Set Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := PL2
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_NAME := lineage_PL2
+PRODUCT_DEVICE := PL2
+PRODUCT_MANUFACTURER := HMD Global
+PRODUCT_BRAND := Nokia
+PRODUCT_MODEL := Nokia 6.1
+
+PRODUCT_GMS_CLIENTID_BASE := android-hmd
+
+TARGET_VENDOR_PRODUCT_NAME := PL2
+TARGET_VENDOR_DEVICE_NAME := PL2
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE=PL2_sprout \
+    PRODUCT_NAME=Plate2_00WW \
+    PRIVATE_BUILD_DESC="Plate2_00WW-user 9 PPR1.180610.011 00WW_3_260 release-keys"
+
+BUILD_FINGERPRINT := Nokia/Plate2_00WW/PL2_sprout:9/PPR1.180610.011/00WW_3_260:user/release-keys
