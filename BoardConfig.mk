4659f5f1d604113386e7407b870807b159828f9a
Aayush Gupta <aayushgupta219@gmail.com>
2021-04-24 06:40:57 +0000

    PL2: Import manifest entry for nfc
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Ib4ced4469bea0f65de1c31694bf2efe31fdd5715

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,9 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm630
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 TARGET_KERNEL_CONFIG := nokia_defconfig
 
