a227758ec9066f13aa8f78ec50d50eb64cd0bb7e
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    PL2: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_PL2.mk
+++ lineage_PL2.mk
@@ -6,7 +6,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Device
 $(call inherit-product, device/nokia/PL2/device.mk)
 
-BUILD_FINGERPRINT := Nokia/Plate2_00WW/PL2_sprout:10/QKQ1.190828.002/00WW_4_15O:user/release-keys
 PRODUCT_BRAND := Nokia
 PRODUCT_DEVICE := PL2
 PRODUCT_MANUFACTURER := HMD Global
@@ -14,6 +13,8 @@ PRODUCT_MODEL := Nokia 6.1
 PRODUCT_NAME := lineage_PL2
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=PL2_sprout \
-    PRODUCT_NAME=Plate2_00WW \
-    PRIVATE_BUILD_DESC="Plate2_00WW-user 10 QKQ1.190828.002 00WW_4_15O release-keys"
+    BuildDesc="Plate2_00WW-user 10 QKQ1.190828.002 00WW_4_15O release-keys" \
+    BuildFingerprint=Nokia/Plate2_00WW/PL2_sprout:10/QKQ1.190828.002/00WW_4_15O:user/release-keys \
+    DeviceName=Plate2_00WW \
+    DeviceProduct=PL2_sprout \
+    SystemName=PL2
