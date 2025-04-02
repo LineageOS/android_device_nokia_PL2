c5ada0b268b4cbc9aa4466c25c79be9ff8f943a9
Tuan Anh <tuan73176@gmail.com>
2025-04-02 09:40:59 +0700

    PL2: Downgrade to P camera stack
    For whatever reason happening on the Android 10 camera stack that Nokia
    cooked, capture photos with flash is indeed, broken since HAL3 was enforced.
    
    Downgrade back to Android P camera stack seems to solve the issue. While at it,
    disable ELF check for a few libraries that rely on compiler intrinsic symbols.
    
    Also drop Megvii shim on PL2 blobs because the P camera blobs does not rely on it.
    
    Change-Id: I5784916d2f6c79f8b88816cb3727324ae33c2500

--- extract-files.py
+++ extract-files.py
@@ -29,10 +29,6 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
-    'vendor/lib/hw/camera.sdm660.so': blob_fixup()
-	.remove_needed('libMegviiFacepp.so')
-	.remove_needed('libmegface-new.so')
-	.add_needed('libshim_megvii.so'),
      ('vendor/lib/libmmcamera_faceproc.so', 'vendor/lib/libmmcamera_faceproc2.so'): blob_fixup()
         .clear_symbol_version('__aeabi_memcpy')
         .clear_symbol_version('__aeabi_memset')
