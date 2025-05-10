8b7e456203dbd992fd980bca23cefda48041f578
Tuan Anh <tuan73176@gmail.com>
2025-05-10 09:28:25 +0700

    PL2: Build FM2
    Reflecting common device tree changes Iab4f6aceb52665e59db4d4ba6c348ba6a95a1fa9.
    
    Change-Id: I085dd8f2f8135c09167e40a003ad4ec0ac0e903c

--- device.mk
+++ device.mk
@@ -34,6 +34,10 @@ TARGET_SCREEN_WIDTH := 1080
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# FM
+PRODUCT_PACKAGES += \
+    FM2
+
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-hmd
 
