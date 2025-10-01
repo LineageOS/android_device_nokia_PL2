1ec133bc751f45f869b7aa1b34440d8a73d82c02
Tuan Anh <tuan73176@gmail.com>
2025-10-01 14:06:15 +0000

    PL2: Stop hardcoding patchelf version
    Change-Id: Ib499fa088fe6a45f125df0a1ec81aeca4992f37b

--- extract-files.py
+++ extract-files.py
@@ -18,9 +18,6 @@ from extract_utils.main import (
     ExtractUtilsModule,
 )
 
-import extract_utils.tools
-extract_utils.tools.DEFAULT_PATCHELF_VERSION = '0_9'
-
 namespace_imports = [
     'device/nokia/sdm660-common',
     'hardware/qcom-caf/wlan',
